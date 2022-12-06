3d492b441ca9b5ede4384b40c394e8a3a154796c
YntepostmaE <ynte.postma@gmail.com>
2022-12-06 14:13:30 +0100

    fetching questions and categories

---
+++ src/store/Room/thunks.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const getQuestions =
+  (limit, categories, diff) => async (dispatch, getState) => {
+    const response = await axios.get(
+      `https://the-trivia-api.com/api/questions?categories=${categories}&limit=${limit}&region=NL&difficulty=${diff}`
+    );
+    console.log(response);
+  };
+
+export const getCategories = () => async (dispatch, getState) => {
+  const response = await axios.get(`https://the-trivia-api.com/api/categories`);
+  console.log(response);
+};
