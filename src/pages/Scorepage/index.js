9ca8cf8d9b6f94fc594245a9921df7f88117edf7
AdneCode <super-gijs@hotmail.com>
2022-12-07 19:42:16 +0100

    feat: previousAnswer

--- src/pages/Scorepage/index.js
+++ src/pages/Scorepage/index.js
@@ -1,21 +1,34 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectRoom } from "../../store/Room/selectors";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectRoom } from '../../store/Room/selectors';
 
 export const Scorepage = () => {
-  const room = useSelector(selectRoom);
-  const playerArray = room?.players;
-  const sortedPlayerArray = [...playerArray].sort((a, b) => (b.score - a.score));
-  console.log("playerArray", playerArray);
-
-  return (
-    <div>
-      <h1>Player Ranking:</h1>
-      <ol>
-        { sortedPlayerArray.map((player) => {
-          return <li>{ player.name } score: { player.score }</li>;
-        }) }
-
-      </ol>
-    </div>
-  );
+    const room = useSelector(selectRoom);
+    const previousAnswer = room.previousAnswer.answer;
+    const playerArray = room?.players;
+    const sortedPlayerArray = [...playerArray].sort(
+        (a, b) => b.score - a.score,
+    );
+    console.log('playerArray', playerArray);
+    console.log('room', room);
+    return (
+        <div>
+            <h1>
+                The answer to the previous question was
+                {' ' + previousAnswer}
+            </h1>
+            <h1>Player Ranking:</h1>
+            <ol>
+                {sortedPlayerArray.map((player) => {
+                    return (
+                        <li>
+                            {player.name} score: {player.score} Previous answer:
+                            {previousAnswer === player.previousAnswer
+                                ? `GREEN ${player.previousAnswer}`
+                                : ` RED ${player.previousAnswer}`}
+                        </li>
+                    );
+                })}
+            </ol>
+        </div>
+    );
 };
