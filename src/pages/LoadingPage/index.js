be0f808739244ff5016ad258c79a1d057a90a15f
cspr-reijnen <cspr.reijnen@gmail.com>
2022-12-07 21:33:32 +0100

    Add loading styling

--- src/pages/LoadingPage/index.js
+++ src/pages/LoadingPage/index.js
@@ -1,39 +1,47 @@
-import { useContext, useEffect, useDispatch } from 'react';
-import { useSelector } from 'react-redux';
-import Lottie from 'react-lottie';
-import { useNavigate } from 'react-router-dom';
-import runningGameBoy from './runningGameBoy.json';
-import { SocketContext } from '../../socket';
-import { selectRoom } from '../../store/Room/selectors';
+import { useContext, useEffect, useDispatch } from "react";
+import { useSelector } from "react-redux";
+import Lottie from "react-lottie";
+import { useNavigate } from "react-router-dom";
+import runningGameBoy from "./runningGameBoy.json";
+import retro from "./retro.json";
+import { SocketContext } from "../../socket";
+import { selectRoom } from "../../store/Room/selectors";
+import "./style.scss";
+
 export const LoadingPage = () => {
-    const navigate = useNavigate();
-    const socket = useContext(SocketContext);
-    const room = useSelector(selectRoom);
-    console.log("room", room);
+  const navigate = useNavigate();
+  const socket = useContext(SocketContext);
+  const room = useSelector(selectRoom);
+  console.log("room", room);
 
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: runningGameBoy,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-        },
-    };
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: retro,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-    const startGame = () => {
-        socket.emit('startGame', room.roomId);
-    };
+  const startGame = () => {
+    socket.emit("startGame", room.roomId);
+  };
 
-    return (
-        <div>
-            <Lottie options={ defaultOptions } />
-            <button onClick={ startGame }>Start quiz</button>
-            <h1>join with code: { room.roomId }</h1>
-            { room.players.map(player => {
-                return (
-                    <p>{ player.name } joined the game</p>
-                );
-            }) }
+  return (
+    <div className="loading">
+      <Lottie options={defaultOptions} />
+      <div className="loading__wrapper">
+        <h1>join with code: {room.roomId}</h1>
+        <p>Current players:</p>
+        <div className="loading__players">
+          {room.players.map((player) => {
+            return <p className="player">{player.name}</p>;
+          })}
         </div>
-    );
+        <button onClick={startGame} className="btn btn-success">
+          Start quiz
+        </button>
+      </div>
+    </div>
+  );
 };
