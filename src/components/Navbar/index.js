750617d3759757aae99ccc230615dc813bede89d
cspr-reijnen <cspr.reijnen@gmail.com>
2022-12-07 15:46:29 +0100

    Add welcome page styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "./style.scss";
 
 export const AppNavbar = () => {
   return (
     <div>
       <Navbar bg="light">
         <Container>
-          <Navbar.Brand href="#home">Quest Mongers 62</Navbar.Brand>
+          <Link to="/">
+            <Navbar.Brand className="brand">Quest Mongers 62</Navbar.Brand>
+          </Link>
         </Container>
       </Navbar>
     </div>
--- src/pages/Welcomepage/index.js
+++ src/pages/Welcomepage/index.js
@@ -1,57 +1,56 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
-import './styles.scss';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import "./styles.scss";
 import { Link } from "react-router-dom";
 
 //Remove on release:
-import { useContext } from 'react';
-import { SocketContext } from '../../socket';
+import { useContext } from "react";
+import { SocketContext } from "../../socket";
 
 export const Welcomepage = () => {
-    //This can be removed on release:
-    const socket = useContext(SocketContext);
-    const testData = () => {
-        socket.emit('getData');
-    };
-    //end of remove on release, there is still the button
-    return (
-        <div>
-            <Container className="welcome">
-                <Row>
-                    <Col className="text-center">
-                        {/* Remove on release: */ }
-                        <button onClick={ () => testData() }>SERVER</button>
-                        <h1 className="mb-4">Welcome!</h1>
-                        <p className="mb-4">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
-                        </p>
-                        <div className="mb-2 pr-4">
-                            <Stack gap={ 3 } className="col-md-5 mx-auto">
+  //This can be removed on release:
+  const socket = useContext(SocketContext);
+  const testData = () => {
+    socket.emit("getData");
+  };
+  //end of remove on release, there is still the button
+  return (
+    <div className="bg-patterns">
+      <Container className="welcome">
+        <Row>
+          <Col className="text-center">
+            {/* Remove on release: */}
+            {/* <button onClick={() => testData()}>SERVER</button> */}
+            <h1 className="mb-4">Build your quiz!</h1>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </p>
+            <div className="mb-2 pr-4">
+              <Stack gap={3} className="col-md-5 mx-auto">
+                <Link to="/host">
+                  <Button variant="success" className="btn">
+                    Start a game
+                  </Button>
+                </Link>
 
-                                <Link to="/host">
-                                    <Button variant="secondary">Start a game</Button>
-                                </Link>
-
-                                <Link to="/join">
-                                    <Button variant="secondary">Join a game</Button>
-                                </Link>
-
-                            </Stack>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+                <Link to="/join">
+                  <Button variant="outline-success" className="btn">
+                    Join a game
+                  </Button>
+                </Link>
+              </Stack>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
