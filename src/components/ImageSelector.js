dd87aae886dc1caf1555454f702b19497383ac1a
cspr-reijnen <cspr.reijnen@gmail.com>
2022-12-08 11:07:18 +0100

    Add image avatar

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -1,27 +1,27 @@
-import { images } from './images';
-import { useEffect } from 'react';
+import { images } from "./images";
+import { useEffect } from "react";
 
 //ImageBox, each pictured image, including the onClick event
 const ImageBox = (p) => {
-    const { image, setImage } = p;
-    return (
-        <div onClick={() => setImage(image)}>
-            <img src={`${image}`} style={{ height: '271px', width: '271px' }} />
-        </div>
-    );
+  const { image, setImage } = p;
+  return (
+    <div onClick={() => setImage(image)}>
+      <img src={`${image}`} style={{ height: "40px", width: "40px" }} />
+    </div>
+  );
 };
 
 export const ImageSelector = (p) => {
-    const { setImage } = p;
-    useEffect(() => {
-        //random image as first image
-        setImage(images[Math.floor(Math.random() * images.length)]);
-    }, []);
-    return (
-        <div>
-            {images.map((i) => {
-                return <ImageBox image={i} setImage={setImage} />;
-            })}
-        </div>
-    );
+  const { setImage } = p;
+  useEffect(() => {
+    //random image as first image
+    setImage(images[Math.floor(Math.random() * images.length)]);
+  }, []);
+  return (
+    <div className="avatar">
+      {images.map((i) => {
+        return <ImageBox image={i} setImage={setImage} />;
+      })}
+    </div>
+  );
 };
